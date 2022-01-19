779ec125874fe395f11f24c05c0d9b61a18bb1da
Avantika <avantikaab1@gmail.com>
2022-01-19 16:16:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,118 @@
+const form = document.getElementById('form');
+const firstname = document.getElementById('fname');
+const lastname = document.getElementById('lname');
+const email = document.getElementById('email');
+const mobile = document.getElementById('mobile');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	checkInputs();
+});
+
+
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const firstnameValue = firstname.value.trim();
+  const lastnameValue = lastname.value.trim();
+  const mobileValue = mobile.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+
+  function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+  }
+  
+  function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+  }
+	
+	if(firstnameValue === '') {
+    setErrorFor(firstname, "Enter a valid name")
+  } 
+  else {
+		setSuccessFor(firstname);
+	}
+
+  if(lastnameValue === '') {
+		setErrorFor(lastname, "Enter a valid name");
+	} 
+  else {
+		setSuccessFor(lastname);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, "Enter a valid email");
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+
+  function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  }
+
+  if(mobileValue===''){
+    setErrorFor(mobile, "Enter a valid mobile number")
+  }else if(mobileValue.length!=10){
+    setErrorFor(mobile, "Mobile Number should be of 10 digits")
+  }else{
+    setSuccessFor(mobile)
+  }
+	
+	if(passwordValue === '') {
+		setErrorFor(password, "Password can't be blank");
+	}
+  else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, "Password can't be blank");
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Password does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+
+    var list1 = [];
+		var list2 = [];
+		var list3 = [];
+		var list4 = [];
+
+		var n = 1;
+		var x = 0;
+
+function AddRow(){
+
+  var AddRown = document.getElementById('show');
+  var NewRow = AddRown.insertRow(n);
+
+  list1[x] = document.getElementById("fname").value;
+  list2[x] = document.getElementById("lname").value;
+  list3[x] = document.getElementById("email").value;
+  list4[x] = document.getElementById("mobile").value;
+
+  var cell1 = NewRow.insertCell(0);
+  var cell2 = NewRow.insertCell(1);
+  var cell3 = NewRow.insertCell(2);
+  var cell4 = NewRow.insertCell(3);
+
+  cell1.innerHTML = list1[x];
+  cell2.innerHTML = list2[x];
+  cell3.innerHTML = list3[x];
+  cell4.innerHTML = list4[x];
+
+  n++;
+  x++;
+}
+
+}
